d7db4fded67d8fe12d84c3b6117a95b903c2c56b
DevMan3158 <e.giner@codeur.online>
2022-09-08 17:02:33 +0200

    Ajout CatPost et Post Fixtures

---
+++ src/DataFixtures/PostFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Post;
+use DatetimeImmutable;
+use App\DataFixtures\CatPostFixtures;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+
+
+
+class PostFixtures extends Fixture implements DependentFixtureInterface
+{
+    //Ajout d'une fonction pour le haché le mot de passe 
+    //public const CHOCOLATERIE_REFERENCE = 'user-robin';
+
+
+
+    /*public const USER_REFERENCE = 'user-gary';*/
+    public function load(ObjectManager $manager): void
+    {
+
+     
+
+        for ($i=0; $i < 5 ; $i++) { 
+        
+        $Post = new Post();
+        $Post->setContenu("Ceci est le contenu du post");
+        $Post->setCreatedAt(new DatetimeImmutable());
+        $Post->setImagePost("https://via.placeholder.com/150");
+        $Post->setImagePostAlt("https://via.placeholder.com/150");
+        $Post->setCatPost($this->getReference('categorie_'.$i));
+
+
+        $manager->persist($Post);
+        }
+
+
+        $manager->flush();
+    }
+    
+        
+        //$this->addReference(self::USER_REFERENCE, $user);
+
+        public function getDependencies()
+        {
+            return array(
+                CatPostFixtures::class,
+            );
+        }
+}

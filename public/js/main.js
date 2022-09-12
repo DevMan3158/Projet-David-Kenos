25a731fb3d600bb975398c6d099d16cb182b8edf
loloxdev <loris_labarre@outlook.fr>
2022-09-12 11:31:47 +0200

    Page 1 back_office_admin presque terminée

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+var headdd = document.getElementById('head');
+var ul = document.querySelectorAll('ul');
+
+function openMenu(){
+    headdd.classList.add("headerBurger");
+}
+
+function closeMenu(){
+    headdd.classList.remove("headerBurger");
+}
+

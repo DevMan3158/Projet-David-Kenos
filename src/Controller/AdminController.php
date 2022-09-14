3ebcbb91055b7d6cb55964615cdf7fcc3b923264
loloxdev <loris_labarre@outlook.fr>
2022-09-14 16:57:39 +0200

    Modif structure

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,21 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BackAdminController extends AbstractController
+class AdminController extends AbstractController
 {
     #[Route('admin/back', name: 'app_back_admin')]
     public function index(): Response
     {
-        return $this->render('back_admin/index.html.twig', [
-            'controller_name' => 'BackAdminController',
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 
     #[Route('admin/crud', name: 'app_back_crud')]
     public function crud(): Response
     {
-        return $this->render('back_admin/crud.html.twig', [
-            'controller_name' => 'BackAdminController',
+        return $this->render('admin/crud.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 }

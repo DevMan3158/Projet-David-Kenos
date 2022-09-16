51debeffba09d0a68e6cbee9404243e6e7da1a07
DevMan3158 <e.giner@codeur.online>
2022-09-16 08:52:54 +0200

    Ajout de la page profil - en cours

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('utilisateur/mon-espace', name: 'app_user')]
-    public function index(): Response
+    #[Route('utilisateur/mon-espace', name: 'app_user', methods:['GET']) ]
+    public function index(Request $request, UserRepository $userRepository  ): Response
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+
+          
+            'userRepository' => $userRepository->findAll(),
         ]);
     }
 }

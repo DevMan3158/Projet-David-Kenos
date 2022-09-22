0c3c920ba9307d96b6c8ef124057fcb6cedea75c
DevMan3158 <e.giner@codeur.online>
2022-09-22 12:00:15 +0200

    modules less - ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,6 @@ use App\Repository\PostRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\PersistentCollection;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Repository\CommentaireRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,8 +21,8 @@ class UserController extends AbstractController
     
             public function index(Request $request,ManagerRegistry $doctrine, UserRepository $userRepository, PostRepository $postRepository ): Response
     {
-        $user = $this->getUser()->getId();
-        $post = $postRepository->findBy(array('id' => $user));
+        $user = $this->getUser()->getUserIdentifier();
+        $post = $postRepository->findAll($user);
         
         return $this->render('user/profil_view/index.html.twig', [
 

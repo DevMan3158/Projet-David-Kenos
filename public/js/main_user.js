3b716e49a13ca77f30beb554c51b0491e6473d2c
loloxdev <loris_labarre@outlook.fr>
2022-09-27 13:58:16 +0200

    Trombinoscope terminé

--- public/js/main_user.js
+++ public/js/main_user.js
@@ -16,5 +16,19 @@ function closeNav() {
 }
 
 
-
+// Fonction permettant de filtrer les utilisateurs du trombinoscope
+function search() {
+  let input = document.getElementById('searchBar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('card');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
 

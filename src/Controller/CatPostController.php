8c62608c19ead2082dee25a0092d71e6934d7794
DevMan3158 <e.giner@codeur.online>
2022-09-29 18:10:59 +0200

    Crud CatPost ok

--- src/Controller/CatPostController.php
+++ src/Controller/CatPostController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('admin/cat/post')]
+#[Route('admin/categories')]
 class CatPostController extends AbstractController
 {
     #[Route('/', name: 'app_cat_post_index', methods: ['GET'])]

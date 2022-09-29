791899041189855f41de1ab999e4095542310005
loloxdev <loris_labarre@outlook.fr>
2022-09-29 10:24:59 +0200

    maj fixtures et css accueil

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -29,18 +29,26 @@ class UserFixtures extends Fixture implements DependentFixtureInterface
         //Tableau
 
         $tableUser=[
-        ['nom' => 'Kenos', 'prenom' =>'David' ,'poste' => 'Gérant des chocolateries','email' => 'David.Kenos@gmail.fr', 'roles' => ["ROLE_SUPER_ADMIN"], 'chocolaterie' => 1 ,'ImageProfil'=>'../img/profils/david_kenos/david-kenos-child.jpeg','ImageBandeau'=>'.../img/profils/david_kenos/gateau-chocolats.jpeg'],
+
+        ['nom' => 'Kenos', 'prenom' =>'David' ,'poste' => 'Gérant des chocolateries','email' => 'David.Kenos@gmail.fr', 'roles' => ["ROLE_SUPER_ADMIN"], 'chocolaterie' => 1 ,'ImageProfil'=>'../img/profils/david_kenos/david-kenos-child.jpeg','ImageBandeau'=>'../img/profils/david_kenos/gateau-chocolats.jpeg'],
+
         ['nom' => 'Alebat', 'prenom' =>'Evelyne ' ,'poste' => 'Chargée de clientèle ','email' => 'Evelyne.Alebat@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 1  ,'ImageProfil'=>'../img/profils/evelyne_alebat/woman-blond-smile.jpeg','ImageBandeau'=>'../img/profils/evelyne_alebat/paysage-montagne-bandeau.jpeg'],
+
         ['nom' => 'Allemand', 'prenom' =>'Arthur' ,'poste' => 'Gérant','email' => 'Arthur.Allemand@gmail.fr', 'roles' => ["ROLE_ADMIN"], 'chocolaterie' => 2  ,'ImageProfil'=>'../img/profils/arthur_allemand/arthur-allemand-homme.jpeg','ImageBandeau'=>'../img/profils/arthur_allemand/cities-skyline.jpeg'],
+
         ['nom' => 'Mylenas', 'prenom' =>'Romain' ,'poste' => 'Chargé de clientèle','email' => 'Romain.Mylenas@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 1  ,'ImageProfil'=>'../img/profils/romain_mylenas/romain-mylenas-chemise.jpeg','ImageBandeau'=>'../img/profils/romain_mylenas/ville-mer-montagne.jpeg'],
-        ['nom' => 'Ousbema', 'prenom' =>'Sukaina' ,'poste' => 'Chocolatière','email' => 'Sukaina.Ousbema@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 3  ,'ImageProfil'=>'../img/profils/anthony_riviere/anthony-riviere.jpg','ImageBandeau'=>'../img/profils/anthony_riviere/foret-bandeau-profil.jpeg'],
-        ['nom' => 'Treik', 'prenom' =>'Eliott' ,'poste' => 'Chocolatier ','email' => 'Eliott.Treik@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 1  ,'ImageProfil'=>'../img/profils/anthony_riviere/anthony-riviere.jpg','ImageBandeau'=>'../img/profils/anthony_riviere/foret-bandeau-profil.jpeg'],
-        ['nom' => 'Raoul', 'prenom' =>'Marvin' ,'poste' => 'Chocolatier ','email' => 'Marvin.Raoul@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 2  ,'ImageProfil'=>'../img/profils/anthony_riviere/anthony-riviere.jpg','ImageBandeau'=>'../img/profils/anthony_riviere/foret-bandeau-profil.jpeg'],
-        ['nom' => 'Espinosa', 'prenom' =>'Leslie' ,'poste' => 'Chocolatière','email' => 'leslie.espinosa@gmail.com', 'roles' => ['ROLE_USER'], 'chocolaterie' => 4  ,'ImageProfil'=>'../img/profils/anthony_riviere/anthony-riviere.jpg','ImageBandeau'=>'../img/profils/anthony_riviere/foret-bandeau-profil.jpeg'],
-        ['nom' => 'Chevallier', 'prenom' =>'Louis' ,'poste' => 'Chocolatier','email' => 'louis.chevalier@gmail.com', 'roles' => ['ROLE_USER'], 'chocolaterie' => 0  ,'ImageProfil'=>'../img/profils/anthony_riviere/anthony-riviere.jpg','ImageBandeau'=>'../img/profils/anthony_riviere/foret-bandeau-profil.jpeg'],
+
+        ['nom' => 'Ousbema', 'prenom' =>'Sukaina' ,'poste' => 'Chocolatière','email' => 'Sukaina.Ousbema@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 3  ,'ImageProfil'=>'../img/profils/sukaina_ousbema/sukaina-ousbema-femme.jpeg','ImageBandeau'=>'../img/profils/sukaina_ousbema/flowers-pink-butterfly.jpeg'],
+
+        ['nom' => 'Treik', 'prenom' =>'Eliott' ,'poste' => 'Chocolatier ','email' => 'Eliott.Treik@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 1  ,'ImageProfil'=>'../img/profils/eliott_treik/man-lunettes.jpeg','ImageBandeau'=>'../img/profils/eliott_treik/fleurs-oranges-rose.jpeg'],
+
+        ['nom' => 'Raoul', 'prenom' =>'Marvin' ,'poste' => 'Chocolatier ','email' => 'Marvin.Raoul@gmail.fr', 'roles' => ["ROLE_USER"], 'chocolaterie' => 2  ,'ImageProfil'=>'../img/profils/marvin_raoul/homme-lumieres.jpeg','ImageBandeau'=>'../img/profils/marvin_raoul/panorama-landscape.jpeg'],
+
+        ['nom' => 'Espinosa', 'prenom' =>'Leslie' ,'poste' => 'Chocolatière','email' => 'leslie.espinosa@gmail.com', 'roles' => ['ROLE_USER'], 'chocolaterie' => 4  ,'ImageProfil'=>'../img/profils/leslie_espinosa/femme-chapeau.jpeg','ImageBandeau'=>'../img/profils/leslie_espinosa/island.jpeg'],
+
+        ['nom' => 'Chevallier', 'prenom' =>'Louis' ,'poste' => 'Chocolatier','email' => 'louis.chevalier@gmail.com', 'roles' => ['ROLE_USER'], 'chocolaterie' => 0  ,'ImageProfil'=>'../img/profils/louis_chevallier/homme-jeune.jpeg','ImageBandeau'=>'../img/profils/louis_chevallier/city-skyline-homme.jpeg'],
+
         ['nom' => 'Riviere', 'prenom' =>'Anthony' ,'poste' => 'Gérant','email' => 'anthony.riviere@gmail.com', 'roles' => ['ROLE_ADMIN'], 'chocolaterie' => 1 ,'ImageProfil'=>'../img/profils/anthony_riviere/anthony-riviere.jpg','ImageBandeau'=>'../img/profils/anthony_riviere/foret-bandeau-profil.jpeg'],
-        
-     
 
         ];
 
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -71,31 +71,6 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         return $qb->getQuery()->getSingleScalarResult();
     }
 
-/*    public function findAllWithChoco()
-{
-    $rawSql = "SELECT * FROM user INNER JOIN chocolaterie on user.chocolaterie_id = chocolaterie.id LIMIT 0,5";
-
-    $stmt = $this->getEntityManager()->getConnection()->prepare($rawSql);
-    $stmt->executeQuery([]);
-
-    $stmt->fetchAllAssociative();
-}*/
-
-/*public function findAllWithChoco()
-    {
-
-    $query = $this->createQuery(
-        "SELECT *
-         FROM user
-         INNER JOIN chocolaterie
-         ON user.chocolaterie_id = chocolaterie.id
-         LIMIT 0,5");
- 
-    // returns an array of Product objects
-    return $query->getQuery()
-                ->getResult();
-         
-    }*/
 
 //    /**
 //     * @return User[] Returns an array of User objects

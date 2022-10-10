9f385369622326f2b9c6105f38856c7b6bc138f5
DevMan3158 <e.giner@codeur.online>
2022-10-10 17:15:58 +0200

    Fonctionnalité ajout d'image profil -> ok

--- src/Controller/UserProfilController.php
+++ src/Controller/UserProfilController.php
@@ -23,98 +23,82 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 class UserProfilController extends AbstractController
 {
 
-
-   /* #[Route('user/{id}', name: 'app_user_profil_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, Int $id, UserRepository $userRepository, ChocolaterieRepository $chocolaterieRepository): Response
-    {
-        $form = $this->createForm(User1Type::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-        
-            $userRepository->add($user, true);
-            //Permet de raffraichir la même page 
-           // $user = $userRepository->find($id);
-
-
-            //Récupère l'id dans id et l'intègre à la route 
-           // return $this->redirectToRoute('app_mon_profil', ['id' => $id], Response::HTTP_SEE_OTHER);
-            return $this->redirectToRoute('app_mon_profil', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('user_profil/edit.html.twig', [
-            'user' => $user,
-            'form' => $form,
-        ]);*/
-
-
-
         #[Route('user/{id}', name: 'app_user_profil_edit', methods: ['GET', 'POST'])]
         public function edit(EntityManagerInterface $em, Request $request ,SluggerInterface $slugger, User $user, Int $id, UserRepository $userRepository, ChocolaterieRepository $chocolaterieRepository): Response
         {
 
-
-            //dd($request);            
-            $id = $this->getUser();
-            //dd($userId);
             $form = $this->createForm(User1Type::class, $user);
 
             $form->handleRequest($request);
     
-
-            //  cette condition est nécessaire pour les champs du formulaire hors fichier
+        //  cette condition est nécessaire pour les champs du formulaire hors Upload
 
         if ($form->isSubmitted() && $form->isValid()) 
         {
-
+            
             $userRepository->add($user, true);
 
             /** @var UploadedFile $imageFile */
 
+            //Récupère la donnée du champs ImageBandeau du usertype et le stoke
             $imageFile = $form->get('ImageBandeau')->getData();
-            
-            
-            // cette condition est nécessaire car le champ 'ImageBandeau' n'est pas obligatoire
 
-            // donc le fichier doit être traité uniquement lorsqu'un fichier est téléchargé
+            //Récupère la donnée du champs ImageProfil du usertype et le stoke
+            $imageFilePro = $form->get('ImageProfil')->getData();
+
+            //Cette condition est nécessaire car le champ 'ImageBandeau' n'est pas obligatoire
+            //
+            //Donc le fichier doit être traité uniquement lorsqu'un fichier est téléchargé
             if ($imageFile) {
                 $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
                 
-            // ceci est nécessaire pour inclure en toute sécurité le nom du fichier dans l'URL
+            //Ceci est nécessaire pour inclure en toute sécurité le nom du fichier dans l'URL
                 $safeFilename = $slugger->slug($originalFilename);
                 
-            //on indique le chemin du fichier pour l'enregistrement dans la bdd   
+            //On indique le chemin du fichier pour l'enregistrement dans la bdd et donne un id unique au fichier  
                 $newFilename = '../data/'.$safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
 
             /* Définit data_directory dans le fichier service.yaml    
-                Déplace le fichier dans le répertoire où sont stockées les images*/
-
+               Déplace le fichier dans le répertoire où sont stockées les fichiers*/
                 try {
                     
                     $imageFile->move($this->getParameter('data_directory'),$newFilename);
 
+            //Gérer l'exception si quelque chose se passe pendant le téléchargement du fichier
                 } catch (FileException $e) {
                     
-            // ... gérer l'exception si quelque chose se passe pendant le téléchargement du fichier
                 }
 
-                
-            // met à jour la propriété 'setImageBandeau' pour stocker le nom du fichier 
-            // au lieu de son contenu
+            //Met à jour la propriété 'setImageBandeau' pour stocker le nom du fichier 
             $user->setImageBandeau($newFilename);
+
             }
 
-            // ... persister la variable $user ou tout autre travail
-            //hydrate la bdd
+            //Conditionne dans le cas d'un changement de l'image de profile 
+            elseif ($imageFilePro) {
+                $originalFilename = pathinfo($imageFilePro->getClientOriginalName(), PATHINFO_FILENAME);
+                
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = '../data/'.$safeFilename.'-'.uniqid().'.'.$imageFilePro->guessExtension();
+                try {
+                    
+                    $imageFilePro->move($this->getParameter('data_directory'),$newFilename);
+
+                } catch (FileException $e) {
+                    
+                }
+
+            $user->setImageProfil($newFilename);
+            }
+
+            // Persiste la variable $user ou tout autre travail
             $em->persist($user);
 
+            // Hydrate la bdd
             $em->flush();
 
                 $this->addFlash('success', 'Leaderboard modifié');
-
-                /*return $this->redirectToRoute('app_mon_profil',
-                 [
-                    'id' => $user = $this->getUser()], Response::HTTP_SEE_OTHER);*/
+                //return $this->redirectToRoute('app_mon_profil',['id' => $id], Response::HTTP_SEE_OTHER);
             }
     
             return $this->renderForm('user_profil/edit.html.twig', [
@@ -122,8 +106,8 @@ class UserProfilController extends AbstractController
                 'form' => $form,
             ]);
 
-
     }
+
 
     #[Route('/{id}', name: 'app_user_profil_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, UserRepository $userRepository): Response
